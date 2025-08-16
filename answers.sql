7e40ba3b671ba0b304dd43a7f6d4f4adc6bda888
lynn004 <kurwalynn@gmail.com>
2025-08-16 22:34:37 +0300

    Add answers.sql file

--- answers.sql
+++ answers.sql
@@ -3,3 +3,18 @@ CREATE DATABASE salesDB;
 
 -- 2. Drop (delete) the database called demo
 DROP DATABASE demo;
+--3.  retrieve the checkNumber, paymentDate, and amount from the payments table.
+
+USE DATABASE salesDB;
+SELECT checkNumber, paymentDate, amount
+FROM payments;
+
+--4. retrieve the orderDate, requiredDate, and status of orders currently 'In Process' from the orders table. 
+--Sort the results in descending order of orderDate
+USE salesDB;
+
+SELECT orderDate, requiredDate, status
+FROM orders
+WHERE status = 'In Process'
+ORDER BY orderDate DESC;
+
