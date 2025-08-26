a94c2d3b9083559240c2d2e7e6043255d8479cb7
lynn004 <kurwalynn@gmail.com>
2025-08-27 00:55:56 +0300

    Keep only answers.sql

--- README.md
+++
@@ -1,4 +0,0 @@
-
-# Elite-School-Website
-index.html assignment
- a16204d054cb19ecf07eccafb8cc315d092e1142
--- sets2.py
+++
@@ -1,4 +0,0 @@
-admins = {"lynn","ava","mark","audrey","owen","cate"}
-for admin in admins:
-    print (("admin:", admin))
-    
